24a426ea14954dc4dcd2a8c8aa441782a3c09f45
Lucas Iron <lucas.iron@hotmail.com.br>
2023-01-01 23:27:07 -0300

    test(length): centimeter to meter conversion

--- length/tests.py
+++ length/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class TestLengthConversion(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('length:DjangoReact')
+
+    def test_centimeter_to_meter_conversion(self):
+        data = {
+            'input_unit': 'centimeter',
+            'output_unit': 'meter',
+            'input_value': round(8096.894, 3)
+        }
+
+        response = self.client.get(self.url, data)
+
+        self.assertContains(response, 80.969)
